7ea8bd87b4751f3fa9da4f041bba2bd84c930d9c
canned-slammin <sd.burner@gmail.com>
2025-06-27 12:41:55 -0700

    LVGL example compiles (untested on HW)

--- app/src/main.c
+++ app/src/main.c
@@ -1,5 +1,99 @@
 #include "main.h"
+/*BEGIN lvgl example includes*/
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
+#include <lvgl.h>
+#include <stdio.h>
+#include <string.h>
+#include <zephyr/kernel.h>
+#include <lvgl_input_device.h>
+/*END lvgl example includes*/
+
+/* BEGIN lvgl example setup and globals*/
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app);
+
+static uint32_t count;
+
+/* END lvgl example setup and globals*/
+
+/*BEGIN lvgl example callbacks*/
+
+static void lv_btn_click_callback(lv_event_t *e)
+{
+	ARG_UNUSED(e);
+
+	count = 0;
+}
+
+/*END lvgl example callbacks*/
 
 int main(void) {
+
+    /*BEGIN lvgl example variable init*/
+
+    char count_str[11] = {0};
+	const struct device *display_dev;
+	lv_obj_t *hello_world_label;
+	lv_obj_t *count_label;
+
+    /*END lvgl example variable init*/
+
+    /*BEGIN lvgl example get display*/
+
+    display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device not ready, aborting test");
+		return 0;
+	}
+
+    /*END lvgl example get display*/
+
+    /*BEGIN lvgl example initialization*/
+
+    /*create graphics objects*/
+	if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN) || IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
+		lv_obj_t *hello_world_button;
+
+		hello_world_button = lv_btn_create(lv_scr_act());
+		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, -15);
+		lv_obj_add_event_cb(hello_world_button, lv_btn_click_callback, LV_EVENT_CLICKED,
+				    NULL);
+		hello_world_label = lv_label_create(hello_world_button);
+	} else {
+		hello_world_label = lv_label_create(lv_scr_act());
+	}
+
+    /*initialize graphics objects*/
+	lv_label_set_text(hello_world_label, "Hello world!");
+	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
+
+	count_label = lv_label_create(lv_scr_act());
+	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+    /*start lvgl task handler and turn off display blanking*/
+	lv_task_handler();
+	display_blanking_off(display_dev);
+
+    /*END lvgl example initialization*/
+
+    /*BEGIN lvgl example main loop*/
+
+    while (1) {
+		if ((count % 100) == 0U) {
+			sprintf(count_str, "%d", count/100U);
+			lv_label_set_text(count_label, count_str);
+		}
+		lv_task_handler();
+		++count;
+		k_sleep(K_MSEC(10));
+	}
+
+    /*END lvgl example main loop*/
+
     return 0;
 }
