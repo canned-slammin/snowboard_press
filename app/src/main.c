43e56c868c9d753ec0a8a7b2b78f7c53b5b01138
canned-slammin <sd.burner@gmail.com>
2025-07-01 12:50:57 -0700

    buttons and alert show up on screen

--- app/src/main.c
+++ app/src/main.c
@@ -17,7 +17,9 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app);
 
+/*
 static uint32_t count;
+*/
 
 /* END lvgl example setup and globals*/
 
@@ -33,14 +35,14 @@ static uint8_t alert = 0;
 /*END subsystem testing setup and globals*/
 
 /*BEGIN lvgl example callbacks*/
-
+/*
 static void lv_btn_click_callback(lv_event_t *e)
 {
 	ARG_UNUSED(e);
 
 	++count;
 }
-
+*/
 /*END lvgl example callbacks*/
 
 /*BEGIN subsystem testing callbacks*/
@@ -69,20 +71,27 @@ int main(void) {
 
     /*BEGIN lvgl example variable init*/
 
+	/*
     char count_str[11] = {0};
 	char* beer_str = {"beers I'm going to drink tonight:"};
-	const struct device *display_dev;
 	lv_obj_t *hello_world_label;
 	lv_obj_t *count_label;
 	lv_obj_t *beers;
+	*/
 
     /*END lvgl example variable init*/
 
 	/*BEGIN subsystem testing variable init*/
-	char alert_str[4] = {0};
+	const struct device *display_dev;
+
+	char* alert_str = {"OFF"};
+	char* relay1_str = {"Toggle Relay 1"};
+	char* relay2_str = {"Toggle Relay 2"};
+
+	lv_obj_t *alert_label;
 	lv_obj_t *relay1_label;
 	lv_obj_t *relay2_label;
-	lv_obj_t *alert_label;
+
 	/*END subsystem testing variable init*/
 
     /*BEGIN lvgl example get display*/
@@ -112,9 +121,18 @@ int main(void) {
 
 	lv_obj_t *relay1_button;
 	relay1_button = lv_btn_create(lv_scr_act());
-	lv_obj_align(relay1_button, LV_ALIGN_LEFT_MID, 5, 0);
+	lv_obj_align(relay1_button, LV_ALIGN_CENTER, 0, 45);
 	lv_obj_add_event_cb(relay1_button, lv_relay1_callback, LV_EVENT_CLICKED, NULL);
 	relay1_label = lv_label_create(relay1_button);
+
+	lv_obj_t *relay2_button;
+	relay2_button = lv_btn_create(lv_scr_act());
+	lv_obj_align(relay2_button, LV_ALIGN_CENTER, 0, 0);
+	lv_obj_add_event_cb(relay2_button, lv_relay2_callback, LV_EVENT_CLICKED, NULL);
+	relay2_label = lv_label_create(relay2_button);
+
+	alert_label = lv_label_create(lv_scr_act());
+	lv_obj_align(alert_label, LV_ALIGN_CENTER, 0, -45);
 
     /*initialize graphics objects*/
 	/*
@@ -128,6 +146,14 @@ int main(void) {
 	lv_obj_align(beers, LV_ALIGN_BOTTOM_MID, 0, -20);
 	lv_label_set_text(beers, beer_str);
 	*/
+
+	lv_label_set_text(relay1_label, relay1_str);
+	lv_obj_align(relay1_label, LV_ALIGN_CENTER, 0, 0);
+
+	lv_label_set_text(relay2_label, relay2_str);
+	lv_obj_align(relay2_label, LV_ALIGN_CENTER, 0, 0);
+
+	lv_label_set_text(alert_label, alert_str);
 
     /*start lvgl task handler and turn off display blanking*/
 	lv_task_handler();
