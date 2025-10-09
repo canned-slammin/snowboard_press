d5ea931d3398472bf99511f16bd99d7270889022
canned-slammin <sd.burner@gmail.com>
2025-10-09 12:42:58 -0700

    outline for testing bacnet comms

--- app/src/console_cmds.c
+++ app/src/console_cmds.c
@@ -5,3 +5,12 @@
 #include <ctype.h>
 #include <string.h>
 
+/*TODO bacnet read_int*/
+
+/*TODO bacnet write_int*/
+
+/*TODO bacnet read_float*/
+
+/*TODO bacnet write_float*/
+
+/*TODO create bacnet test command subset*/
